75fa05e41de0d336c2e2193c8cb4c3369dbcb67b
psi <ryo.hirafuji@link-u.co.jp>
2020-01-27 16:42:51 +0900

    add comment about workaround

--- make-deb.sh
+++ make-deb.sh
@@ -7,6 +7,7 @@ export PATH="$(python3 -c 'import site; print(site.USER_BASE);')/bin:${PATH}"
 
 fakeroot debian/rules clean
 fakeroot debian/rules binary
+# workaround. external/libpng will be dirty after making debian packages.
 env --chdir=external/libpng git reset --hard
 mv ../davif_*.deb ../davif-dbgsym_*.ddeb .
 
