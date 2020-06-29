df312fefd7cc1de303853501e6892b8d7c4cafa2
psi <ryo.hirafuji@link-u.co.jp>
2020-06-29 22:35:27 +0900

    Improve test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -eux
 
 set -eux
+set -o pipefail
 
 BASE_DIR=$(cd $(dirname $(readlink -f $0)) && cd .. && pwd)
 cd ${BASE_DIR}
@@ -9,6 +10,9 @@ apt install -y ./artifact/*.deb
 apt show davif
 which davif
 
-davif --help
+davif || true
+if [ $? -ne 0 ]; then
+  exit -1
+fi
 
 ldd $(which davif)
